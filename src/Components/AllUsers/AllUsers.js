c9b536fac235cf54aa387b872e537bb78c3ea19b
Sashank Tungaturthi <devil.sas.007@gmail.com>
2019-11-19 17:59:19 -0700

    Fix mobile view for players

--- src/Components/AllUsers/AllUsers.js
+++ src/Components/AllUsers/AllUsers.js
@@ -2,14 +2,27 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Coin from "../../Assets/coin_img.png";
 import Musician from "../../Assets/dj.png";
+import Drawer from "@material-ui/core/Drawer";
+import { Button } from "@material-ui/core";
+
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 import "./AllUsers.css";
 export default function AllUsers() {
+  const matches = useMediaQuery("(min-width:600px)");
+
   let { playingUsers } = useSelector(state => ({
     ...state.sessionReducer,
     ...state.privateIdReducer,
     ...state.allUsersReducer
   }));
+
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false
+  });
 
   const GetAllUsers = () => {
     if (playingUsers) {
@@ -22,18 +35,68 @@ export default function AllUsers() {
     }
   };
 
+  const toggleDrawer = (side, open) => event => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [side]: open });
+  };
+
   return (
-    <div className="allusers-container">
-      <p className="current-players-heading">CURRENT PLAYERS</p>
-      <ul className="users-list">
-        <li>
-          <span>
-            <img className="coin" src={Coin} alt="points"></img>
-            <img className="coin" src={Musician} alt="musician"></img>
-          </span>
-        </li>
-        <GetAllUsers />
-      </ul>
+    <div>
+      {matches ? (
+        <div className="allusers-container">
+          <p className="current-players-heading">CURRENT PLAYERS</p>
+          <ul className="users-list">
+            <li className="img-item">
+              <span>
+                <img className="coin" src={Coin} alt="points"></img>
+                <img className="coin" src={Musician} alt="musician"></img>
+              </span>
+            </li>
+            <GetAllUsers />
+          </ul>
+        </div>
+      ) : (
+        <div>
+          <Button
+            style={{
+              fontSize: 8,
+              fontFamily: "'Rajdhani', sans-serif",
+              position: "absolute",
+              left: 0,
+              marginLeft: "12px",
+              top: "10vh",
+              color: "white"
+            }}
+            onClick={toggleDrawer("left", true)}
+            variant="outlined"
+          >
+            Scoreboard
+          </Button>
+          <Drawer
+            style={{ width: 250 }}
+            anchor="left"
+            open={state.left}
+            onClose={toggleDrawer("left", false)}
+          >
+            <p className="current-players-heading">CURRENT PLAYERS</p>
+            <ul className="users-list">
+              <li className="img-item">
+                <span>
+                  <img className="coin" src={Coin} alt="points"></img>
+                  <img className="coin" src={Musician} alt="musician"></img>
+                </span>
+              </li>
+              <GetAllUsers />
+            </ul>
+          </Drawer>
+        </div>
+      )}
     </div>
   );
 }
